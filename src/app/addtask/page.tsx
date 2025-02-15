aca3785cfc84b5003b2d777d68990c8c20ff6896
Megat Abdul Haq <dnmegatmehat@gmail.com>
2025-02-15 10:20:44 +0800

    Added initial form

--- src/app/addtask/page.tsx
+++ src/app/addtask/page.tsx
@@ -1,7 +1,24 @@
+"use client";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-export default function AddTask () {
-    return <div>This is the add task page.
-        <Link href={'/'}>Home</Link>
-    </div>
+export default function AddTask() {
+	type TaskInput = {
+		taskName: string;
+	};
+
+	const { register, handleSubmit } = useForm<TaskInput>();
+
+	const onSubmit: SubmitHandler<TaskInput> = (data) => console.log(data);
+
+	return (
+		<div className="flex flex-col w-screen h-screen items-center justify-center">
+			<h1>Add a task!</h1>
+			<form onSubmit={handleSubmit(onSubmit)} className="text-black">
+				<input defaultValue={"Task"} {...register("taskName")} />
+				<input type="submit" className="bg-white" />
+			</form>
+			<Link href={"/"}>Home</Link>
+		</div>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
-      <div>Number of items in task list.</div>
-      <Link href={'/addtask'}>Add tasks</Link>
-    </div>
-  );
+	return (
+		<div>
+			<div>Number of items in task list.</div>
+			<Link href={"/addtask"}>Add tasks</Link>
+		</div>
+	);
 }
